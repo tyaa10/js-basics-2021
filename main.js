e432a90acd25c46ce5b4d367aae123aa159fb8b6
YuriiTrofimenko <tyaa@ukr.net>
2021-04-16 18:15:25 +0300

    init n demo: variables n types

---
+++ main.js
@@ -0,0 +1,65 @@
+/* Constants n variables */
+
+// const four = 4
+/* const FOUR_NUMBER = 4
+
+console.log('Hello "JS"!')
+console.log("Hello 'JS'! - 2")
+console.log("Hello 'JS'! -" + " 3")
+console.log(`Hello 'JS'! - ${FOUR_NUMBER}`) */
+
+/* var x = 100
+x = '100'
+
+if (true) {
+  var x = 911
+  // x = 911
+}
+
+console.log(`x = ${x}`) */
+
+/* let x = 100
+x = '100'
+// let x = 911
+
+if (true) {
+  let x = 911
+  console.log(`x = ${x}`)
+}
+
+console.log(`x = ${x}`) */
+
+
+/* Embedded types */
+
+/* x = 100
+// let x = 100
+if (true) {
+  let x = 911
+  console.log(`x = ${x}`)
+}
+console.log(`x = ${x}`) */
+
+// number, string, boolean, undefined, null, bigint
+// object
+
+// console.log(y)
+
+// const y
+let y
+
+console.log(y)
+console.log(y + 1)
+
+// console.log(3 + 1 == 3 + 1)
+// console.log(3 + 1 == 4)
+// console.log(3 + 1 == '4')
+// console.log(3 + 1 === '4')
+// console.log(3 + 1 === 4)
+// console.log(y + 1 == y + 1)
+// console.log(y + 1 === y + 1)
+console.log(Number.isNaN(y + 1) === Number.isNaN(y + 1)) // true === true
+console.log(100 / 0)
+console.log(100 / -0)
+const z = null
+// const z = undefined
